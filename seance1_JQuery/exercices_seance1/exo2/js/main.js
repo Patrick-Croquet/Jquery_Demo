307e9eb867fd4ac0c55e8c2ca1e29fad88439db7
Patrick Croquet <patrick.croquet@wanadoo.fr>
2020-11-24 11:15:11 +0100

    Solutions exercices séance 1

---
+++ seance1_JQuery/exercices_seance1/exo2/js/main.js
@@ -0,0 +1,4 @@
+$("#paragraphe1").add("<p>Hello</p>").appendTo(document.body);
+//$("#paragraphe1").add("<p>Hello</p>").appendTo("#paragraphe1");
+//$("#paragraphe1").add("<p>Hello</p>").appendTo("#paragraphe2");
+//$("#paragraphe1").add("<p>Hello</p>").appendTo("#paragraphe1");
---
+++ seance1_JQuery/exercices_seance1/exo6/js/main.js
@@ -0,0 +1,3 @@
+$("button").click(function(){
+    $(".para").after("<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium labore, adipisci maiores voluptas minima a natus porro dicta veritatis aut in eos fugiat illo corrupti est distinctio veniam fuga aspernatur.</p>");
+});
